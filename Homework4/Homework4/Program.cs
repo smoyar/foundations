e36f5eb08755e2433fd88b305cbeba52b0520418
s_moyar <sergio.moya.roman@gmail.com>
2016-12-09 21:51:41 -0800

    Homework4 Calculate the factorial of a given number and show the results in between

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Looping: Write a program that calculates factorial of an integer n given by the user.  Please enter a small integer.
+            //Asignment #3
+
+            double fact = 1;
+            Console.Write("Please enter a number to calculate its factorial: ");
+            string strnum = Console.ReadLine();
+            int n = int.Parse(strnum);
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                fact = i * fact;
+                Console.WriteLine("{0}! = {1}", i, fact);
+            }
+            Console.ReadLine();
+        }
+    }
+}
